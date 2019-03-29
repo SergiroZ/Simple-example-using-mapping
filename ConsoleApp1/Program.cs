1341fe2610597004aa3be021df2a38a83f92e4bc
Sergiro Z <zujonok@hotmail.com>
2019-03-29 10:57:57 +0300

    Using mapping

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -3,13 +3,46 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using AutoMapper;
 
 namespace ConsoleApp1
 {
-    class Program
+    public class UserDto
     {
-        static void Main(string[] args)
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Email { get; set; }
+    }
+
+    public class User
+    {
+        public string FullName { get; set; }
+
+        public string Email { get; set; }
+    }
+
+
+    internal class Program
+    {
+        private static void Main()
         {
+
+            Mapper.Initialize(cfg => cfg.CreateMap<UserDto, User>()
+                                        .ForMember("Name",
+                                                   opt => opt.MapFrom(c => c.FirstName +
+                                                                           " " + c
+                                                                               .LastName))
+                                        .ForMember("Email",
+                                                   opt => opt.MapFrom(src => src.Email)));
+
+            var userDto = new UserDto
+                {FirstName = "Alex", LastName = "Koh", Email = "alex@gmail.com"};
+
+            User user = Mapper.Map<UserDto, User>(userDto);
+
+            Console.WriteLine($"{user.FullName}+: {user.Email}");
         }
     }
 }
